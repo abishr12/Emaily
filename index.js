393853dd45abee3f7aa6a9a7b689acc582fb19c8
abishr <adhambishr@gmail.com>
2018-04-03 16:58:14 -0700

    create passport

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express();
 
+passport.use(new GoogleStrategy())
+
+// clientID
+// 527715501916-etfd86d8ug2vg94jtetelm289v4i88dr.apps.googleusercontent.com
+// clientSecret KiaHlK7aG1JK10jm9VJVvNFC
 app.get('/', (req,res)=>{
     res.send({hi: 'there'})
 });
